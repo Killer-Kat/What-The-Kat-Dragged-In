a9ac9815442d6b55ec8be4a28ea9214748ca09aa
Killer-Kat <goldkat99@gmail.com>
2019-12-05 01:16:24 -0500

    Update Letter_Grade.py
    Updated, added plus and minus grades.

--- Letter_Grade.py
+++ Letter_Grade.py
@@ -3,12 +3,26 @@
 #So I wrote this for fun, feel free to use it as long as you arent Apple.
 grade = float(input("Enter Your Grade: "))
 
-if grade >= 90:
+if grade >= 93:
  print ("Your grade is A!")
-elif grade >= 80:
+elif grade >= 90:
+ print ("Your grade is A-")
+elif grade >= 87:
+ print ("Your grade is B+")
+elif grade >= 83:
  print ("Your grade is B")
-elif grade >= 70:
+elif grade >= 80:
+ print ("Your grade is B-")
+elif grade >= 77:
+ print ("Your grade is C+")
+elif grade >= 73:
  print ("Your grade is C")
+elif grade >= 70:
+ print ("Your grade is C-")
+elif grade >= 67:
+ print ("Your grade is D+")
+elif grade >= 63:
+ print ("Your grade is D")
 elif grade >= 60:
  print ("Your grade is D")
 elif grade >= 0:
