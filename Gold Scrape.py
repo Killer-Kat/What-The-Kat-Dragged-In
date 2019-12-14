ba3bb176c8db30051e5ed4d90ab932278b340c01
Killer-Kat <goldkat99@gmail.com>
2019-12-14 13:51:49 -0500

    Nugget Conversion 2.0! now gets the spot price of gold from the web.
    I have added code to scrape the web for the spot price of gold. This will allow accurate calculations. Future goals include scraping the spot price of chicken nuggets and implementing a GUI or other user interface.

--- Gold Scrape.py
+++ Gold Scrape.py
@@ -4,8 +4,8 @@ import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-my_url = "https://www.jmbullion.com/charts/gold-price/"
 #this part gets the web page and scrapes it for its html data.
+my_url = "https://www.jmbullion.com/charts/gold-price/"
 gold_url = urlopen(my_url)
 page_html = gold_url.read()
 gold_url.close()
@@ -15,5 +15,5 @@ containers = page_soup.findAll("div", {"id": "gounce"})
 #this prints the output and extracts the price of gold as a float.
 print(containers)
 gold_price = float("".join(filter(lambda d: str.isdigit(d) or d == '.', str(containers)))) #dont know how this works, it was written by thund on stack overflow
-print(gold_price)
+print("the valvue of gold is $" + str(gold_price) + " per Troy ounce")
 input("end?")
--- Nugget_conversion.py
+++ Nugget_conversion.py
@@ -1,31 +1,42 @@
 #Gold to chicken nugget conversion.
-#Version 1.0, Made by Killer-Kat
+#Version 2.0, Made by Killer-Kat
 #In order to measure the conversion between gold nuggets and chicken nuggets we need to establish a few things.
 #Gold is measured in troy ounces and one troy ounce is 1.09714 regular ounces.
 #In this instance we are going to convert on a one to one mass ratio.
 #As of 12/6/2019 McNuggets are 40 for $8.99 USD and they average 0.5 ounces per nugget.
 #Thats 0.455729 Troy Ounces for our gold nuggets.
+import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+#this part gets the web page and scrapes it for its html data.
+my_url = "https://www.jmbullion.com/charts/gold-price/"
+gold_url = urlopen(my_url)
+page_html = gold_url.read()
+gold_url.close()
+#This converts the raw HTML data into a float that the script can use for its calculations.
+page_soup = BeautifulSoup(page_html, "html.parser")
+containers = page_soup.findAll("div", {"id": "gounce"})
+gold_price = float("".join(filter(lambda d: str.isdigit(d) or d == '.', str(containers)))) #dont know how this works, it was written by thund on stack overflow
+#print("the valvue of gold is $" + str(gold_price) + " per Troy ounce")
 
 Choice = int(input("Choose an option: 1: Gold Nuggets to Chicken Nuggets, 2: Chicken Nuggets to Gold Nuggets. "))
-
-
-Price = 673.37 #This is the price of 0.455729 troy ounces of gold as of 12/6/2019
+gold_nugget_price = (gold_price * 0.455729) #This is the price of 0.455729 troy ounces of gold
 Forty_Piece = 0.22475 #This is the price of one chicken nugget based on the mcdonald's 40 piece meal.
-
+#This part uses the users choice to convert either gold to chicken or chicken to gold.
 if Choice == 1:
  #Gold to chicken Nugget conversion.
  Nuggets = float(input("Amount of Gold Nuggets to Convert "))
- Dollars = Nuggets * Price
+ Dollars = Nuggets * gold_nugget_price
  print("Thats $", end="")
  print (round(Dollars), end="")
  print (" worth of gold.")
  G2C = Dollars / Forty_Piece #This is where the magic happens, and by magic I mean math.
 
- #This just prints the final statement.
  print (str(Nuggets) + " Gold nuggets is worth " + str(round(G2C)) + " Chicken Nuggets" )
 elif Choice == 2:
  Nuggets = float(input("Amount of Chicken Nuggets to Convert "))
  Dollars = Nuggets * Forty_Piece
- C2G = Dollars / Price
+ C2G = Dollars / gold_nugget_price
  print(C2G)
 input("Press Enter to close")
