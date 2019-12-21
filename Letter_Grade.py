075ce746d387ca9914ff404913075b9b26b9c412
Killer-Kat <goldkat99@gmail.com>
2019-12-21 02:23:51 -0500

    Letter Grade Version 2
    Added GUI, reworked code.

--- Letter_Grade.py
+++ Letter_Grade.py
@@ -1,15 +1,16 @@
 #Simple program to turn a score into a letter grade. Made by Killer Kat.
 #I offhandedly tried asking siri to tell me what the grade corresponding to a percent was and I was suprised that it didnt.
 #So I wrote this for fun, feel free to use it as long as you arent Apple.
+#Version 2, added gui and other improvements.
+from tkinter import *
 
-print ("This tool will calculate your final grade in a class based on your current score.")
-#this calculates a percentage based on user input
 def Grade_Percent():
+#this calculates a percentage based on user input
  try:
-  Current_Points = float(input("Input current Points in class: "))
-  Total_Points = float(input("Input total points possible: "))
+  Current_Points = float(my_score.get())
+  Total_Points = float(max_score.get())
   if Total_Points == 0:
-   print ("If the maxium points available are zero that means you passed.") 
+   #print ("If the maxium points available are zero that means you passed.") 
    percent = 100
   else:
    percent = ((Current_Points / Total_Points) * 100)
@@ -18,38 +19,60 @@ def Grade_Percent():
   percent = 0
  finally:
   grade = percent
-  print ("You got %" + str(round(percent, 2)))
+  GradeMSG = ("You got %" + str(round(percent, 2)))
 
  #These if statemnts print what grade you got.
  if grade >= 100:
-  print ("Your grade is A+, Impressive!")
+  labelText.set( GradeMSG + ": Your grade is A+, Impressive!")
  elif grade >= 93:
-  print ("Your grade is A!")
+  labelText.set(GradeMSG + ": Your grade is A!")
  elif grade >= 90:
-  print ("Your grade is A-")
+  labelText.set(GradeMSG + ": Your grade is A-")
  elif grade >= 87:
-  print ("Your grade is B+")
+  labelText.set(GradeMSG + ": Your grade is B+")
  elif grade >= 83:
-  print ("Your grade is B")
+  labelText.set(GradeMSG + ": Your grade is B")
  elif grade >= 80:
-  print ("Your grade is B-")
+  labelText.set(GradeMSG + ": Your grade is B-")
  elif grade >= 77:
-  print ("Your grade is C+")
+  labelText.set(GradeMSG + ": Your grade is C+")
  elif grade >= 73:
-  print ("Your grade is C")
+  labelText.set(GradeMSG + ": Your grade is C")
  elif grade >= 70:
-  print ("Your grade is C-")
+  labelText.set(GradeMSG + ": Your grade is C-")
  elif grade >= 67:
-  print ("Your grade is D+")
+  labelText.set(GradeMSG + ": Your grade is D+")
  elif grade >= 63:
-  print ("Your grade is D")
+  labelText.set(GradeMSG + ": Your grade is D")
  elif grade >= 60:
-  print ("Your grade is D")
+  labelText.set(GradeMSG + ": Your grade is D")
  elif grade >= 0:
-  print ("Your grade is an F, That means you fail. You need %" + str(round((60 - percent),2)) + " more to pass.")
+  labelText.set(GradeMSG + ": Your grade is an F") # You need %" + str(round((60 - percent),2)) + " more to pass.")
  else:
-  print ("ERROR")
+  labelText.set("ERROR")
 
-Grade_Percent()
 
-input("Press Enter to close")
+#GUI
+root = Tk()
+root.title("Grade Calculator")
+root.geometry("325x150")
+root.resizable(0,0)
+root.configure(background="DeepSkyBLue4")
+
+label_1 = Label(root, text="Current Score:", bg="DeepSkyBLue4", fg="LightSteelBlue2")
+label_1.grid(row=0, sticky=W)
+my_score = Entry(root, bg="DeepSkyBLue3", fg="LightSteelBlue2")
+my_score.grid(row=0, column=1, sticky=W)
+label_2 = Label(root, text="Total Possible Score:", bg="DeepSkyBLue4", fg="LightSteelBlue2")
+label_2.grid(row=1, sticky=W)
+max_score = Entry(root,bg="DeepSkyBLue3", fg="LightSteelBlue2")
+max_score.grid(row=1, column=1, sticky=W)
+
+button_1 = Button(root, text="Calculate Grade", command=Grade_Percent, bg="DeepSkyBLue4", fg="LightSteelBlue2")
+button_1.grid(row=3)
+#This is the output
+labelText = StringVar()
+label_3 = Label(root, textvariable=labelText, bg="DeepSkyBLue4", fg="LightSteelBlue2")
+label_3.grid(row=4, columnspan=2)
+
+root.mainloop()
