36562854dcaf43d085b0580a9c7e0ec61e6a8c33
Killer-Kat <goldkat99@gmail.com>
2019-12-20 23:17:27 -0500

    Update Letter_Grade.py
    added Exception handling.

--- Letter_Grade.py
+++ Letter_Grade.py
@@ -4,45 +4,52 @@
 
 print ("This tool will calculate your final grade in a class based on your current score.")
 #this calculates a percentage based on user input
-Current_Points = float(input("Input current Points in class: "))
-Total_Points = float(input("Input total points possible: "))
-if Total_Points == 0:
- print ("If the maxium points available are zero that means you passed.") 
- percent = 100
-else:
- percent = ((Current_Points / Total_Points) * 100)
+def Grade_Percent():
+ try:
+  Current_Points = float(input("Input current Points in class: "))
+  Total_Points = float(input("Input total points possible: "))
+  if Total_Points == 0:
+   print ("If the maxium points available are zero that means you passed.") 
+   percent = 100
+  else:
+   percent = ((Current_Points / Total_Points) * 100)
+ except ValueError:
+  print("Error: Please input a numerical value.")
+  percent = 0
+ finally:
+  grade = percent
+  print ("You got %" + str(round(percent, 2)))
 
-grade = percent
-print ("You got %" + str(round(percent, 2)))
+ #These if statemnts print what grade you got.
+ if grade >= 100:
+  print ("Your grade is A+, Impressive!")
+ elif grade >= 93:
+  print ("Your grade is A!")
+ elif grade >= 90:
+  print ("Your grade is A-")
+ elif grade >= 87:
+  print ("Your grade is B+")
+ elif grade >= 83:
+  print ("Your grade is B")
+ elif grade >= 80:
+  print ("Your grade is B-")
+ elif grade >= 77:
+  print ("Your grade is C+")
+ elif grade >= 73:
+  print ("Your grade is C")
+ elif grade >= 70:
+  print ("Your grade is C-")
+ elif grade >= 67:
+  print ("Your grade is D+")
+ elif grade >= 63:
+  print ("Your grade is D")
+ elif grade >= 60:
+  print ("Your grade is D")
+ elif grade >= 0:
+  print ("Your grade is an F, That means you fail. You need %" + str(round((60 - percent),2)) + " more to pass.")
+ else:
+  print ("ERROR")
 
-#These if statemnts print what grade you got.
-if grade >= 100:
- print ("Your grade is A+, Impressive!")
-elif grade >= 93:
- print ("Your grade is A!")
-elif grade >= 90:
- print ("Your grade is A-")
-elif grade >= 87:
- print ("Your grade is B+")
-elif grade >= 83:
- print ("Your grade is B")
-elif grade >= 80:
- print ("Your grade is B-")
-elif grade >= 77:
- print ("Your grade is C+")
-elif grade >= 73:
- print ("Your grade is C")
-elif grade >= 70:
- print ("Your grade is C-")
-elif grade >= 67:
- print ("Your grade is D+")
-elif grade >= 63:
- print ("Your grade is D")
-elif grade >= 60:
- print ("Your grade is D")
-elif grade >= 0:
- print ("Your grade is an F, That means you fail. You need %" + str(round((60 - percent),2)) + " more to pass.")
-else:
- print ("ERROR")
- 
+Grade_Percent()
+
 input("Press Enter to close")
