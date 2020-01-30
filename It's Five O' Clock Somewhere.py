d49b82605d0e1fd20dbc28a14f50f7a6ffcfdd11
Killer-Kat <goldkat99@gmail.com>
2020-01-30 00:56:04 -0500

    Create It's Five O' Clock Somewhere.py
    Work in progress

---
+++ It's Five O' Clock Somewhere.py
@@ -0,0 +1,16 @@
+# Its 5 o clock somewhere
+#And this program will tell you where.
+
+#Code me something tall and stong, make it a hurricane before I go insane.
+#Its only half compiled but I dont care, its five O' Clock Somewhere!
+
+import time
+import datetime
+
+#Get Current Hour
+now = datetime.datetime.now()
+print("The current hour is " + str(now.hour))
+
+#Get current timezone
+time.tzname
+input()
