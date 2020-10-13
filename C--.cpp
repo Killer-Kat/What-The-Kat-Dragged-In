4bb0fbfde8700aa7ffb2d621d93443b69f2c8a6c
Killer-Kat <goldkat99@gmail.com>
2020-10-12 20:55:12 -0400

    Update C--.cpp

--- C--.cpp
+++ C--.cpp
@@ -1,9 +1,20 @@
-//C--
-//My first C++ program, you know I had to make this joke
-int main() {
+//You Know I had to make this joke be my first C++ program
 
-//User instructions 
-std::cout << "Please input the value of the C variable";
+#include <iostream>
 
+int main()
+{
+    int C = 0;
+
+    std::cout << "Please enter the value of the C variable\n";
+    std::cin >> C;
+
+    //Maths
+    std::cout << "C + C is: " << C+C << "\n";
+    std::cout << "C + C + C is: " << C+C+C << "\n";
+    std::cout << "C x C is: " << C*C << "\n";
+    std::cout << "C - C is: " << C - C << "\n";
+    std::cout << "C - C -C is: " << C - C - C << "\n";
+    
 
 }
