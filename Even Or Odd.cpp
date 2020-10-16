d4ed571b5c9f120854b9b756739c184827711504
Killer-Kat <goldkat99@gmail.com>
2020-10-16 19:03:07 -0400

    V2
    Added random number generation.

--- Even Or Odd.cpp
+++ Even Or Odd.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <vector> // vector support
+#include <cstdlib> // for random number gen
+#include <ctime> // gets time
 
-//This Program checks if the numbers in a vector are even or odd and prints it to console.
+//This Program generates random numbers and then lists if they are even or odd.
 int main() {
+    //seeds the sudo random number generator with the time, not the best choice but it works well enough for this use case.
+    srand((unsigned)time(0));
 
-    std::vector<int> Numbers = { 2, 4, 3, 6, 1, 9, 15, 8, -12, 125, -7, 4};
-    int odd = 0, even = 0;
+    int odd = 0, even = 0, input = 0;
 
+    std::cout << "How many numbers would you like to test? \n";
+    std::cin >> input;
+    
+    //This for loop fills the vector with random numbers.
+    std::vector<int> Numbers(0);
+    for (int x = 0; x < input; x++) {
+        Numbers.push_back((rand() % 1000) + 1);
+    }
+
+    //and this for loop prints them to console after checking if they are even or odd.
     for (int i = 0; i < Numbers.size(); i++) {
         if ((Numbers[i] % 2) != 0) {
         std::cout << Numbers[i] << " is odd \n";
