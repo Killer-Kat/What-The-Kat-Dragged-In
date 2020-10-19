20cf4a1c779e187fbe0f21844ffff0d9775884cb
Killer-Kat <goldkat99@gmail.com>
2020-10-19 03:54:50 -0400

    Update WheelOfBuzzwords GUI.py
    I had to update this after I saw an ad for a machine learnnig smart toothbrush.

--- WheelOfBuzzwords GUI.py
+++ WheelOfBuzzwords GUI.py
@@ -4,9 +4,9 @@
 
 import random
 from tkinter import *
-
+#These lists store the buzzwords and products.
 buzzwords = ["Cloud Native", "Blockchain", "Container Based", "Zero Trust", "Machine Learning", "Augmented Reality", "5G"]
-products = [ "IPS System", "IOT Device", "Smart Fridge", "Teleconferencing Service", "Social Media Network", "Business Collaboration Platform", "VR Headset", "Self Driving Car"]
+products = [ "IPS System", "IOT Device", "Smart Fridge", "Teleconferencing Service", "Social Media Network", "Business Collaboration Platform", "VR Headset", "Self Driving Car", "Electric Toothbrush"]
 
 #print("Hello and wellcome to, WHEEL, OF, BUZZWORDS!!!")
 #print("Silicon Valley's hottest new gameshow!")
@@ -16,9 +16,9 @@ def Spin():
  buzz1 = buzzwords[random.randrange(0, 7, 1)]
  buzz2 = buzzwords[random.randrange(0, 7, 1)]
  if buzz1 == buzz2:
-  print("Looks like you are making a " + buzz1 + " & " + buzz2 + " " + products[random.randrange(0, 8, 1)] + " you must really like "  + buzz1 +"!")
+  print("Looks like you are making a " + buzz1 + " & " + buzz2 + " " + products[random.randrange(0, 9, 1)] + " you must really like "  + buzz1 +"!")
  else: 
-  print ("Looks like you are making a " + buzz1 + " & " + buzz2 + " " + products[random.randrange(0, 8, 1)] + ".")
+  print ("Looks like you are making a " + buzz1 + " & " + buzz2 + " " + products[random.randrange(0, 9, 1)] + ".")
 
 #This part is the GUI
 root = Tk()
