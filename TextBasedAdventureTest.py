8bece085b77df42c1be0a2ed10e4d6ec6a2511e0
Killer-Kat <goldkat99@gmail.com>
2023-01-16 04:54:38 -0500

    Update TextBasedAdventureTest.py
    xyzzy

--- TextBasedAdventureTest.py
+++ TextBasedAdventureTest.py
@@ -128,6 +128,8 @@ def TextParser(text, room):
                     else: print("You cannot go west here.")
             case "hint":
                 Hint()
+            case "xyzzy":
+                print("A hollow voice says, Nerd!")
             case _: 
                 print("The parser didnt recognize your verb, please try again!")
     except IndexError:
@@ -140,7 +142,7 @@ def Main(promt):
     Main(currentRoom.name)
 
 def Hint():
-    hintsList = ["Try going weast.", "XYZZY", "You cant get ye flask!", "You can get a hint by using the Hint verb!", "It's an open source game, just look at the code!", "Try calling our support hotline at 1-800-555-KILLERKAT", "Control alt delete", "Ask again later", "Have you listened to my podcast The CyberKat Cafe? Check out our website cyberkatcafe.com"]
+    hintsList = ["Try going weast.", "XYZZY", "You cant get ye flask!", "You can get a hint by using the Hint verb!", "It's an open source game, just look at the code!", "Try calling our support hotline at 1-800-555-KILLERKAT", "Control alt delete", "Ask again later", "Have you listened to my podcast The CyberKat Cafe? Check out our website cyberkatcafe.com", "That's not a bug, it's a feature!"]
     print(random.choice(hintsList))
 
 Main("Wellcome! To give commands use the format VERB: NOUN")
